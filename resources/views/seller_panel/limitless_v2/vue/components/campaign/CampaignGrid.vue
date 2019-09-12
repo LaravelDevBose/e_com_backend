dbbcc85a44821873cc65cbed4f2eee3b256df3ad
tokingfisher19 <tokingfisher19@gmail.com>
2019-09-12 16:57:53 +0600

    #33 seller panel campaign option added

---
+++ resources/views/seller_panel/limitless_v2/vue/components/campaign/CampaignGrid.vue
@@ -0,0 +1,58 @@
+<template>
+
+    <div class="thumbnail">
+        <div class="thumb">
+            <img :src="campaing.attachment.image_path" alt="">
+            <div class="caption-overflow">
+                <span>
+                    <a href="#" class="btn btn-flat border-white text-white">Details</a>
+                    <a href="#" class="btn btn-flat border-white text-white">Join</a>
+                </span>
+            </div>
+        </div>
+
+        <div class="caption">
+            <div class="content-group-sm media">
+                <div class="media-body">
+                    <h6 class="text-semibold no-margin">
+                        <a href="#" class="text-default">Data Governance</a>
+                    </h6>
+
+                    <small class="text-muted">by <a href="#">Eugene Kopyov</a></small>
+                </div>
+
+                <h6 class="text-success media-right no-margin-bottom text-semibold">$49.99</h6>
+            </div>
+
+            Way extensive and dejection get delivered deficient sincerity gentleman age. Calling offence six joy feeling <a href="#">[...]</a>
+        </div>
+
+        <div class="panel-footer panel-footer-transparent">
+            <div class="heading-elements">
+                <ul class="list-inline list-inline-separate heading-text">
+                    <li><i class="icon-comment position-left"></i> 384</li>
+                </ul>
+
+                <span class="heading-text pull-right">
+                    <i class="icon-star-full2 text-size-base text-warning-300"></i>
+                    <i class="icon-star-full2 text-size-base text-warning-300"></i>
+                    <i class="icon-star-full2 text-size-base text-warning-300"></i>
+                    <i class="icon-star-full2 text-size-base text-warning-300"></i>
+                    <i class="icon-star-full2 text-size-base text-warning-300"></i>
+                    <span class="text-muted position-right">(49)</span>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CampaignGrid",
+        props:['campaign']
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/seller_panel/limitless_v2/vue/store/modules/campaign.js
+++ resources/views/seller_panel/limitless_v2/vue/store/modules/campaign.js
@@ -1,12 +1,34 @@
 //declare State
-const state = {};
+const state = {
+    campaign_list:{},
+};
 
 //declare Getters
-const getters = {};
+const getters = {
+    campaigns:(state)=>state.campaign_list,
+};
 
-const actions = {};
+const actions = {
+    async getCampaignList({commit},formData=null){
+        try {
+            await axios.get('/seller/campaigns',formData)
+                .then(response=>{
+                    if(response.data.code !== undefined && response.data.code === 200){
+                        commit('setCampaignList', response.data.data);
+                    }else{
+                        console.log(response)
+                    }
 
-const mutations = {};
+                })
+        }catch (error) {
+            commit('setResponse', error.data);
+        }
+    },
+};
+
+const mutations = {
+    setCampaignList:(state,response)=>state.campaign_list = response,
+};
 
 export default {
     state,
