891744f57b65baf7dde9f770f7e1219e6a26f862
Laravel Dev Bose <Laravel.devbose@gmail.com>
2019-11-11 12:00:57 +0600

    #46 Shop Details Profile show Done

--- resources/views/templates/crocus_v2/frontend/shop_profile.blade.php
+++ resources/views/templates/crocus_v2/frontend/shop_profile.blade.php
@@ -12,16 +12,36 @@
             <div class="row">
                 <div class="col-md-9">
                     <div class="category-description std">
-                        <img class="" src="{{ asset('crocus_v2/images/listing-banner.png') }}" alt="category-banner">
+                        @if(!empty($shop->banner->image_path))
+                            <img class="" src="{{ $shop->banner->image_path }}" alt="category-banner">
+                        @else
+                            <img class="" src="{{ asset('crocus_v2/images/listing-banner.png') }}" alt="category-banner">
+                        @endif
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="text-widget widget widget__sidebar">
                         <div class="widget-content">
                             <div class="item text-center">
-                                <img class="img img-circle" style="margin: 0 auto;" width="100" height="100" alt="blog image" src="{{ asset('crocus_v2/images/blog-img.jpg') }}">
+                                @if(!empty($shop->shopLogo->image_path))
+                                    <img class="img img-circle" style="margin: 0 auto;" width="100" height="100" alt="blog image" src="{{ $shop->shopLogo->image_path }}">
+                                @else
+                                    <img class="img img-circle" style="margin: 0 auto;" width="100" height="100" alt="blog image" src="{{ asset('crocus_v2/images/blog-img.jpg') }}">
+                                @endif
                             </div>
-                            <h3 class="text text-capitalize text-center" style="margin-top: 3px;"><span>Most Popular Post</span></h3>
+                            <h3 class="text text-capitalize text-center" style="margin-top: 3px;"><span>{{ $shop->shop_name }}</span></h3>
+                            @if(!empty($shop->phone_no))
+                                <p class="shop-address">
+                                    <i class="fa fa-phone"></i>
+                                    <span>{{ $shop->phone_no }}</span>
+                                </p>
+                            @endif
+                            @if(!empty($shop->shop_address))
+                                <p class="shop-address">
+                                    <i class="fa fa-map-marker"></i>
+                                    <span>{!! $shop->shop_address !!}</span>
+                                </p>
+                            @endif
                         </div>
                     </div>
                 </div>
