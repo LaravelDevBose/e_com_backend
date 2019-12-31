95a254583f5ea6f81563ae6c9e8cd4aceb4e5a5c
Laravel Dev Bose <Laravel.devbose@gmail.com>
2019-12-31 13:19:13 +0600

    #70 add core app.js file

--- resources/views/admin_panel/limitless_v2/account/index.blade.php
+++ resources/views/admin_panel/limitless_v2/account/index.blade.php
@@ -6,6 +6,7 @@
 @endsection
 
 @section('ThemeJs')
+    <script type="text/javascript" src="{{ asset('assets/js/core/app.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/plugins/ui/ripple.min.js') }}"></script>
 @endsection
 
