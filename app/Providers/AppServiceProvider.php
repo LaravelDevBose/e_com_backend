2187bad95a72c322bb1809d6e894aa32d75fcd7c
laraveldevbose <laravel.devbose@gmail.com>
2020-01-03 02:21:01 +0600

    #70 footer menu test 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,13 +31,21 @@ class AppServiceProvider extends ServiceProvider
             $categoryTree = CommonData::category_tree_list();
             /*$headerPageMenus = CommonData::pages_menu_list(Page::MENU_SHOW_IN['Footer']);
             $pageMenus = CommonData::pages_menu_list(Page::MENU_SHOW_IN['Header']);*/
-            $footerPageMenus = $pages= Page::isActive()->notShowIn(Page::MENU_SHOW_IN['Header'])
+            $tcPageMenus = Page::isActive()->notShowIn(Page::MENU_SHOW_IN['Header'])
+                ->where('page_cat', Page::PageCategory['Terms & Conditions'])
                 ->select('page_id','menu_title','page_slug','show_in','page_status')
-                ->orderBy('menu_position', 'asc');
+                ->orderBy('menu_position', 'asc')->get();
 
-            $tcPageMenus = $footerPageMenus->where('page_cat', Page::PageCategory['Terms & Conditions'])->get();
-            $ppPageMenus = $footerPageMenus->where('page_cat', Page::PageCategory['Privacy & Policy'])->get();
-            $csPageMenus = $footerPageMenus->where('page_cat', Page::PageCategory['Customer Service'])->get();
+            $ppPageMenus = Page::isActive()->notShowIn(Page::MENU_SHOW_IN['Header'])
+                ->where('page_cat', Page::PageCategory['Terms & Conditions'])
+                ->select('page_id','menu_title','page_slug','show_in','page_status')
+                ->orderBy('menu_position', 'asc')->get();
+
+            $csPageMenus = Page::isActive()->notShowIn(Page::MENU_SHOW_IN['Header'])
+                ->where('page_cat', Page::PageCategory['Terms & Conditions'])
+                ->select('page_id','menu_title','page_slug','show_in','page_status')
+                ->orderBy('menu_position', 'asc')->get();
+            
             $aboutUs = Page::isActive()->where('page_slug', 'about_us')->select('body_content')->first();
 
             $brands = CommonData::brand_list();
