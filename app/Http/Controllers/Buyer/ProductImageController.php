9d7bc79a94eecdd99f6be5793e762458d5235001
laraveDevBose <laravel.devbose@gmail.com>
2020-04-28 13:46:28 +0600

    app image store method added

--- app/Http/Controllers/Buyer/ProductImageController.php
+++ app/Http/Controllers/Buyer/ProductImageController.php
@@ -15,10 +15,12 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use Intervention\Image\Facades\Image;
 
 class ProductImageController extends Controller
 {
     private $attachmentFolder = 'public/attachments/';
+    private $appAttachmentFolder = 'public/mobile/attachments/';
 
     public function store(Request $request) {
         // return $request->all();
@@ -129,6 +131,129 @@ class ProductImageController extends Controller
                 'message' => ($message != null) ? $message :'Invalid File!'
             ]);
         }
+    }
+    public function mobile_apps_product_image_store(Request $request) {
+
+        // return $request->all();
+        $attachments = $request->except(['folder']);
+        if(empty($attachments)){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No file found!'
+            ]);
+        }
+
+        $attachmentsArray=['attachment_file' =>[]];
+        foreach ($attachments as $key => $attachment){
+            array_push($attachmentsArray['attachment_file'],  $attachment);
+        }
+
+
+
+        $validator = Validator::make(
+            $attachmentsArray, [
+            'attachment_file.*' => 'required'
+        ],[
+                'attachment_file.*.required' => 'Please upload an image',
+                'attachment_file.*.mimes' => 'Only Images, Doc, Text, PDF are allowed',
+                'attachment_file.*.max' => 'Sorry! Maximum allowed size for an image is 10MB'
+            ]
+        );
+
+        if ($validator->passes()) {
+
+            try {
+
+                $max_number = Attachment::max('attachment_no')+1;
+
+                if(empty($request->folder)){
+                    throw new Exception('Folder is Require!', 400);
+                }
+                $folder = $request->folder;
+
+                $attachmentModels = Attachment::attachmentModels;
+
+
+                if(empty($attachmentModels[$folder])){
+                    throw new Exception('Invalid Model!', 400);
+                }
+
+                $attachmentData= [];
+                foreach ($attachments as $key => $attachment){
+                    $image_array_1 = explode(";", $attachment);
+                    $image_array_2 = explode(",", $image_array_1[1]);
+                    $ImageData = base64_decode($image_array_2[1]);
+
+                    $model = $attachmentModels[$folder];
+                    $flipModels = array_flip($attachmentModels);
+                    $modalName = $flipModels[$model];
+
+                    if($model === $attachmentModels['product'] || $model === $attachmentModels['thumbnail'] ){
+                        $folder = $folder.'/'.$max_number;
+                    }
+
+                    $imageInfo = getimagesizefromstring($ImageData);
+                    $type = $imageInfo['mime'];
+                    $ext = image_type_to_extension($imageInfo[2]);
+                    $name =  md5(rand(1111, 9999). time()).$ext;
+                    $name_full = $this->attachmentFolder . $folder . '/' . $name;
+                    Storage::disk('local')->put( $name_full,$ImageData);
+
+                    $appAttachments = Attachment::appAttachments;
+                    if(!empty($appAttachments[$folder])){
+                        $appImage = $appAttachments[$folder];
+                        $image = Image::make($ImageData);
+                        $image->resize($appImage['width'], $appImage['height']);
+
+                        $name_full = $this->appAttachmentFolder . $folder . '/' . $name;
+                        Storage::disk('local')->put( $name_full, $image->encode());
+                    }
+
+
+                    $attachmentSave = Attachment::create([
+                        'attachment_no' => $max_number,
+                        'reference'     => $model,
+                        'file_name'     => $name,
+                        'folder'        => $folder,
+                        'file_type'     => $type,
+                        'original_name' => null,
+                        'file_size'     => null,
+                        'modal'         => $modalName,
+                    ]);
+
+                    array_push($attachmentData, [
+                        'img' => $attachmentSave->image_path,
+                        'id' => $attachmentSave->attachment_id,
+                    ]);
+                    $max_number++;
+                }
+                return ResponserTrait::allResponse('success', Response::HTTP_OK, 'Image Upload Successfully', $attachmentData);
+
+            }catch (Exception $ex) {
+                DB::rollBack();
+                return response()->json([
+                    'status' => 'error',
+                    'code'=>$ex->getCode(),
+                    'message' => $ex->getMessage()
+                ]);
+            }
+        }else{
+            $errors = array_values($validator->errors()->getMessages());
+            $message = null;
+            foreach ($errors as $error){
+                if(!empty($error)){
+                    foreach ($error as $errorItem){
+                        $message .=  $errorItem .' ';
+                    }
+                }
+            }
+            return response()->json([
+                'status' => 'validation',
+                'code'=> Response::HTTP_BAD_REQUEST,
+                'message' => ($message != null) ? $message :'Invalid File!'
+            ]);
+        }
+
     }
 
     public function download(Attachment $attachment) {
