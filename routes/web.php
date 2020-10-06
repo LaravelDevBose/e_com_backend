e300b72cf16c39fb4c7bd0a3838c566b248103cd
LaravelDevBose <laravel.devbose@gmail.com>
2020-10-07 03:44:58 +0600

    update web route and fixed page 404 error problem

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes(['verify' => true]);
 
 Route::get('/', 'HomeController@index')->name('index');
-Route::any('/{any?}/*', 'HomeController@index')->where('any', '.*');;
+
 
 Route::namespace('Frontend')->prefix('front')->group(function () {
     Route::get('/get-categories', 'FrontendController@category_list');
@@ -20,26 +20,7 @@ Route::namespace('Frontend')->prefix('front')->group(function () {
     Route::post('/get-sidebar-data', 'FrontendController@get_sidebar_data');
     Route::post('/products-list', 'FrontendController@products_list');
 });
-/*Route::namespace('Frontend')->as('front.')->group(function () {
-    Route::get('/set/language/{lang}','FrontendController@set_lang')->name('set.lang');
-    Route::get('/', 'FrontendController@index')->name('index');
-    Route::get('category/{category_slug}/products', 'FrontendController@category_wish_products')->name('category.product');
-    Route::get('product/{product_slug}', 'FrontendController@product_details')->name('product');
-    Route::get('cart', 'FrontendController@cart_page')->name('cart');
-    Route::get('contact', 'FrontendController@contact_pages')->name('contact');
-    Route::get('pages/{slug}', 'FrontendController@general_pages')->name('pages');
 
-    Route::post('/product/sidebar/data', 'FrontendController@product_sidebar_data');
-    Route::post('/shorting/products', 'FrontendController@sorting_product');
-    Route::get('section/list', 'FrontendController@section_data_list')->name('section.list');
-    Route::get('get/section/{section_id}/products', 'FrontendController@get_section_products')->name('section.products');
-    Route::get('/product/variations/data', 'FrontendController@product_variation_data');
-    Route::post('/search', 'FrontendController@searching_data')->name('search');
-    Route::get('/all/category/list', 'FrontendController@get_category_list')->name('category.list');
-    Route::get('/get/hot/products', 'FrontendController@hot_products');
-    Route::get('/shop/{shop_slug}', 'FrontendController@shop_profile')->name('shop.profile');
-    Route::get('/mall', 'FrontendController@mall_products')->name('mall');
-});*/
 
 Route::post('/subscribe', 'NewsLetterController@store');
 
@@ -55,3 +36,4 @@ Route::get('social/login/{provider}', 'SocialLoginController@redirectToProvider'
 Route::get('social/login/{provider}/callback', 'SocialLoginController@handleProviderCallback');
 Route::get('/vue/social/login/{provider}', 'SocialLoginController@handleProviderCallback');
 
+Route::get('/{any}', 'HomeController@index')->where('any', '.*');
