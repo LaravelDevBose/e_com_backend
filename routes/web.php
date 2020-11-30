e31750db6010f403f79e911af21cd63c8ed3ed40
LaravelDevBose <laravel.devbose@gmail.com>
2020-11-30 13:19:12 +0600

    cors test

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+Route::middleware(['cors'])->options('/{any:.*}','HomeController@cors_option_req');
 
 Auth::routes(['verify' => true]);
 
