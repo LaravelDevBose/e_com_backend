b633b18b16e7c0c5cc020d9f00b6c7c3f9397a64
LaravelDevBose <laravel.devbose@gmail.com>
2022-07-23 23:56:01 +0600

    active seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call([
-             AdminsTableSeeder::class,
-             SettingTableSeeder::class,
+             /*AdminsTableSeeder::class,
+             SettingTableSeeder::class,*/
              DefaultSellerSeeder::class,
              DefaultBuyerSeeder::class,
              LatestDealSeeder::class,
